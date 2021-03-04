5457c817746f65511d294ad77aa9467f139d35fd
diko_ua <dlink.uaua@gmail.com>
2021-03-04 11:25:13 +0200

    added saving to file

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,47 @@ struct triangle
     point &operator[](size_t idx) { return vertexes[idx]; }
 };
 
+struct color
+{
+    unsigned char blue;
+    unsigned char green;
+    unsigned char red;
+};
+
+void save_to_file(const std::vector<std::vector<color>> &image, const std::string &file)
+{
+    using namespace std;
+    size_t width = image.size();
+    size_t height = image[0].size();
+
+    size_t filesize = 54 + 3 * width * height;
+    array<unsigned char, 14> bmp_file_header = {'B', 'M', 0, 0, 0, 0, 0, 0, 0, 0, 54, 0, 0, 0};
+    array<unsigned char, 40> bmp_info_header = {40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 24};
+
+    bmp_file_header[2] = (unsigned char)(filesize);
+    bmp_file_header[3] = (unsigned char)(filesize >> 8);
+    bmp_file_header[4] = (unsigned char)(filesize >> 16);
+    bmp_file_header[5] = (unsigned char)(filesize >> 24);
+
+    bmp_info_header[4] = (unsigned char)(width);
+    bmp_info_header[5] = (unsigned char)(width >> 8);
+    bmp_info_header[6] = (unsigned char)(width >> 16);
+    bmp_info_header[7] = (unsigned char)(width >> 24);
+    bmp_info_header[8] = (unsigned char)(height);
+    bmp_info_header[9] = (unsigned char)(height >> 8);
+    bmp_info_header[10] = (unsigned char)(height >> 16);
+    bmp_info_header[11] = (unsigned char)(height >> 24);
+
+    ofstream fout(file);
+    copy(bmp_file_header.begin(), bmp_file_header.end(), ostream_iterator<unsigned char>(fout));
+    copy(bmp_info_header.begin(), bmp_info_header.end(), ostream_iterator<unsigned char>(fout));
+    std::for_each(image.rbegin(), image.rend(), [&fout](const auto &line) {
+        for (auto &i : line)
+            fout << i.blue << i.green << i.red;
+        for (size_t i = 0; i < 4 - 3 * line.size() % 4; i++)
+            fout << 0;
+    });
+}
 
 int main()
 {
