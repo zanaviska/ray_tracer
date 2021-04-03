d90def1fa55ecee495360773305003a5bfb67355
diko_ua <dlink.uaua@gmail.com>
2021-04-03 12:19:09 +0300

    initial commit

---
+++ build/src/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../../bin/project.exe"
+  "../../bin/project.exe.manifest"
+  "../../bin/project.pdb"
+  "CMakeFiles/project.dir/main.cpp.obj"
+  "libproject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
+
+const double eps = 1e-7;
+
+struct point
+{
+    double x;
+    double y;
+    double z;
+};
+
+point operator-(point lhs, point rhs)
+{
+    return {lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z};
+}
+
+point operator*(point lhs, double rhs)
+{
+    return {lhs.x * rhs, lhs.y * rhs, lhs.z * rhs};
+}
+
+struct triangle
+{
+    std::array<point, 3> vertexes;
+    point &operator[](size_t idx) { return vertexes[idx]; }
+};
+
 
 int main()
 {
-    std::cout << "Hello world!\n";
+    
 }
