c1fcdd2934f56dfd493c0e51e1477304db239a10
diko_ua <dlink.uaua@gmail.com>
2021-04-03 12:24:03 +0300

    added reading from file

--- src/main.cpp
+++ src/main.cpp
@@ -34,5 +34,26 @@ struct triangle
 
 int main()
 {
-    
+    // intersect({point{0, 0, 0}, {0, 0, 1}, {0, 1, 0}}, {100, 0, 0}, {0, 5, 5});
+    // return 0;
+    std::ifstream fin("cow.obj");
+    std::string line;
+    std::vector<point> vertexes;
+    std::vector<triangle> triangles;
+    char c;
+    while (fin >> line)
+    {
+        if (line == "v")
+        {
+            double x, y, z;
+            fin >> x >> y >> z;
+            vertexes.push_back({x, y, z});
+        }
+        if (line == "f")
+        {
+            size_t ver1, ver2, ver3;
+            fin >> ver1 >> c >> c >> ver2 >> ver2 >> c >> c >> ver3 >> ver3 >> line;
+            triangles.push_back({{vertexes[ver1], vertexes[ver2], vertexes[ver3]}});
+        }
+    }
 }
