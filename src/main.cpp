3e4d6ff6e3a027245130174635332370c52dc9a9
Andras <dlink.uaua@gmail.com>
2021-04-16 15:11:22 +0300

    everything is ready

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <chrono>
+#include <thread>
+
+using namespace std::chrono;
 
 const double eps = 1e-7;
 
@@ -76,12 +80,20 @@ void save_to_file(const std::vector<std::vector<color>> &image, const std::strin
     ofstream fout(file);
     copy(bmp_file_header.begin(), bmp_file_header.end(), ostream_iterator<unsigned char>(fout));
     copy(bmp_info_header.begin(), bmp_info_header.end(), ostream_iterator<unsigned char>(fout));
-    std::for_each(image.rbegin(), image.rend(), [&fout](const auto &line) {
+    /*std::for_each(image.rbegin(), image.rend(), [&fout](const auto &line) {
         for (auto &i : line)
             fout << i.blue << i.green << i.red;
+        std::cout << line.size() << '\n';
         for (size_t i = 0; i < (4 - 3 * line.size() % 4)%4; i++)
             fout << 0;
-    });
+    });*/
+    for(int j = 0; j < image[0].size(); j++)
+    {
+        for(int i = 0; i < image.size(); i++)
+            fout << image[i][j].blue << image[i][j].green << image[i][j].red;
+        for (size_t i = 0; i < (4 - 3 * image.size() % 4)%4; i++)
+            fout << 0;
+    }
 }
 
 double sqr(double arg)
@@ -124,16 +136,15 @@ std::pair<double, double> intersect(triangle trik, point start, point middle)
     // does triangle contain intersect
     if (std::fabs(area(trik) - area({trik[0], trik[1], inter}) - area({trik[0], trik[2], inter}) - area({trik[2], trik[1], inter})) > eps) 
         return {10000, -2};
-    if(middle.y< -0.5)
-    {
-        std::cout << "------------------------------------------------------------\n";
-    std::cout << std::setprecision(15) << std::fabs(area(trik) - area({trik[0], trik[1], inter}) - area({trik[0], trik[2], inter}) - area({trik[2], trik[1], inter})) << '\n';
-    std::cout << trik[0] << ' ' << trik[1] << ' ' << trik[2] << '\n';
-    std::cout << "intersect: (" << inter.x << ' ' << inter.y << ' ' << inter.z << ")\n";
-    std::cout << area(trik) << ' ' << area({trik[0], trik[1], inter})  << ' ' << area({trik[0], trik[2], inter})  << ' ' <<  area({trik[2], trik[1], inter}) << '\n';
-    }
+    // std::cout << "------------------------------------------------------------\n";
+    // std::cout << start << '\n' << middle << '\n';
+    // std::cout << std::setprecision(15) << std::fabs(area(trik) - area({trik[0], trik[1], inter}) - area({trik[0], trik[2], inter}) - area({trik[2], trik[1], inter})) << '\n';
+    // std::cout << trik[0] << ' ' << trik[1] << ' ' << trik[2] << '\n';
+    // std::cout << "intersect: (" << inter.x << ' ' << inter.y << ' ' << inter.z << ")\n";
+    // std::cout << area(trik) << ' ' << area({trik[0], trik[1], inter})  << ' ' << area({trik[0], trik[2], inter})  << ' ' <<  area({trik[2], trik[1], inter}) << '\n';
+    // std::cin.get();
     
-    return {5, std::fabs(prod1)/(sqrt(sqr(plane_normal.x) + sqr(plane_normal.y) + sqr(plane_normal.z))*sqrt(sqr(ray_normal.x) + sqr(ray_normal.y) + sqr(ray_normal.z)))};
+    return {std::sqrt(sqr(start.x - inter.x) + sqr(start.y - inter.y) + sqr(start.z - inter.z)), std::fabs(prod1)/(sqrt(sqr(plane_normal.x) + sqr(plane_normal.y) + sqr(plane_normal.z))*sqrt(sqr(ray_normal.x) + sqr(ray_normal.y) + sqr(ray_normal.z)))};
 }
 
 int main()
@@ -143,6 +154,7 @@ int main()
     std::vector<point> vertexes;
     std::vector<triangle> triangles;
     char c;
+    auto start = high_resolution_clock::now();
     while (fin >> line)
     {
         if (line == "v")
@@ -155,33 +167,47 @@ int main()
         {
             size_t ver1, ver2, ver3;
             fin >> ver1 >> c >> c >> ver2 >> ver2 >> c >> c >> ver3 >> ver3 >> line;
-            triangles.push_back({{vertexes[ver1], vertexes[ver2], vertexes[ver3]}});
+            triangles.push_back({{vertexes[ver1-1], vertexes[ver2-1], vertexes[ver3-1]}});
+            if(triangles.size() == 4047) std::cout << ver1 << ' ' << ver2 << ' ' << ver3 << '\n';
         }
     }
-    const int64_t height = 100;
-    const int64_t width = 100;
-    std::vector<std::vector<color>> image(height, std::vector<color>(width, {0, 0, 0}));
+    const int64_t height = 720;
+    const int64_t width = 720;
+    std::vector<std::vector<color>> image(width, std::vector<color>(height, {0, 0, 0}));
     std::cout << "image was generated\n";
-    for(int64_t i = 0; i < height && std::cout << i << '\n'; i++)
-        for(int64_t j = 0; j < width; j++)
+    const size_t proc_num = 11;
+    std::vector<std::thread> thrds;
+    for(int64_t i = 0; i < width; i++)
+        thrds.emplace_back([&triangles,&image, i]
         {
-            std::pair<double, double> res = {100000, -2};
-            // std::cout << point({(2.0*i - height)/height, (j*2.0 - width)/width, 0}) << '\n';
-            for(auto &trik: triangles)
+            for(int64_t j = 0; j < height; j++)
             {
-                auto inter = intersect(trik, {0, 0, 100}, {(2.0*i - height)/height, (j*2.0 - width)/width, 0});
-                res = std::min(res, inter);
-                // image[i][j] = {(inter+2)*100, (inter+2)*100, (inter+2)*100};
-            if(res.second != -2 && (j*2.0 - width)/width < -0.5)
-                std::cout << res.second << ' ' << trik[0] << ' ' << trik[1] << ' ' << trik[2] << ' ' << point{(2.0*i - height)/height, (j*2.0 - width)/width, 0} << '\n';
+                std::pair<double, double> res = {100000, -2};
+                // std::cout << point({(2.0*i - height)/height, (j*2.0 - width)/width, 0}) << '\n';
+                // for(int o = 0; o < triangles.size(); o++)
+                for(auto &trik: triangles)
+                {
+                    // auto trik = triangles[o];
+                    // std::cout << o << '\n';
+                    auto inter = intersect(trik, {0, 100, 00}, {(2.0*i - width)/width, 0, (j*2.0 - height)/height});
+                    res = std::min(res, inter);
+                    // image[i][j] = {(inter+2)*100, (inter+2)*100, (inter+2)*100};
+                }
+                if(res.second == -2) res.second = -1;
+                unsigned char color = (1-std::fabs(res.second))*255;
+                image[i][j] = {color, color, color};
+                // image[i][j] = {(unsigned char)(255*(res.second != -2)), (unsigned char)(255*(res.second != -2)), (unsigned char)(255*(res.second != -2))};
+                // image[i][j] = {(unsigned char)((res.second + 2)*80), (unsigned char)((res.second + 2)*80), (unsigned char)((res.second + 2)*80)};
+
+
+                // std::cout << i << ' ' << j << ' ' << (2.0*i - height)/height << ' ' << res.first << ' ' << res.second << '\n';
+
+                // std::cin.get();
             }
-            image[i][j] = {(unsigned char)(255*(res.second != -2)), (unsigned char)(255*(res.second != -2)), (unsigned char)(255*(res.second != -2))};
-            // image[i][j] = {(unsigned char)((res.second + 2)*80), (unsigned char)((res.second + 2)*80), (unsigned char)((res.second + 2)*80)};
-
-
-            // std::cout << i << ' ' << j << ' ' << (2.0*i - height)/height << ' ' << res.first << ' ' << res.second << '\n';
-
-            // std::cin.get();
-        }
+        });
+    for(auto &i: thrds)
+        i.join();
+    auto end = high_resolution_clock::now();
+    std::cout << duration_cast<seconds>(end-start).count() << '\n';
     save_to_file(image, "output.bmp");
 }
