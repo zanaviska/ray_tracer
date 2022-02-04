5add1217127bd4524e41202e4aaab812ebcc5aae
Andras <dlink.uaua@gmail.com>
2022-02-04 18:09:49 +0200

    added threading

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,11 @@
+use std::cmp;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{BufRead, BufReader};
 use std::ops;
 use std::path::Path;
+use std::thread;
+use std::sync::{Arc};
 // use ray_tracer::Color;
 
 #[derive(Copy, Clone, Debug)]
@@ -121,7 +124,7 @@ fn write_to_file(image: Vec<Vec<Vec3>>) -> std::io::Result<()> {
     let height = image.len();
     let width = image[0].len();
 
-    let filesize = 54 + 3*height*width;
+    let filesize = 54 + 3 * height * width;
 
     let mut bmp_file_header: [u8; 14] = [0; 14];
     bmp_file_header[0] = b'B';
@@ -132,7 +135,7 @@ fn write_to_file(image: Vec<Vec<Vec3>>) -> std::io::Result<()> {
     bmp_file_header[5] = (filesize >> 24) as u8;
     bmp_file_header[10] = 54u8;
 
-    let mut bmp_info_header : [u8; 40] = [0; 40];
+    let mut bmp_info_header: [u8; 40] = [0; 40];
     bmp_info_header[0] = 40u8;
 
     bmp_info_header[4] = width as u8;
@@ -143,7 +146,7 @@ fn write_to_file(image: Vec<Vec<Vec3>>) -> std::io::Result<()> {
     bmp_info_header[9] = (height >> 8) as u8;
     bmp_info_header[10] = (height >> 16) as u8;
     bmp_info_header[11] = (height >> 24) as u8;
-    
+
     bmp_info_header[12] = 1u8;
     bmp_info_header[14] = 24u8;
 
@@ -156,26 +159,79 @@ fn write_to_file(image: Vec<Vec<Vec3>>) -> std::io::Result<()> {
         for j in i {
             file.write_all(&[j.x as u8, j.y as u8, j.z as u8])?;
         }
-        file.write_all(&vec![0u8; 3-(len*3-1)%4])?;
+        file.write_all(&vec![0u8; 3 - (len * 3 - 1) % 4])?;
     }
 
     Ok(())
 }
 
-fn main() {
-    let shape = read_file(Path::new("cow.obj"));
-    use std::time::Instant;
-    let now = Instant::now();
+enum NextNode {
+    Node(Node),
+    Triangle(Triangle),
+    Nil,
+}
+
+struct Node {
+    tl: Vec3,
+    br: Vec3,
+    left: Box<NextNode>,
+    right: Box<NextNode>,
+}
+
+struct Tree {
+    root: Box<NextNode>,
+}
+
+impl Tree {
+    pub fn new() -> Tree {
+        Tree {
+            root: Box::new(NextNode::Nil),
+        }
+    }
+    pub fn insert(&mut self, triangle: Triangle) {
+        let return_node = Tree::insert_triangle(&mut self.root, triangle);
+        match *return_node {
+            NextNode::Node(node) => {}
+            NextNode::Triangle(son) => {}
+            _ => {}
+        }
+    }
+
+    fn insert_triangle(next_node: &mut Box<NextNode>, triangle: Triangle) -> Box<NextNode> {
+        let mut new_node = Box::new(NextNode::Nil);
+        let mut return_node = Box::new(NextNode::Nil);
+        match &**next_node {
+            NextNode::Nil => {
+                new_node = Box::new(NextNode::Triangle(triangle));
+            }
+            NextNode::Triangle(_old_triangle) => {
+                return_node = Box::new(NextNode::Triangle(triangle));
+            }
+            NextNode::Node(node) => {
+                println!("node");
+            }
+        }
+        *next_node = new_node;
+        return return_node;
+    }
+}
+
+fn get_image_part(
+    x_min: f32,
+    x_max: f32,
+    y_min: f32,
+    y_max: f32,
+    height: i32,
+    width: i32,
+    shape: &Vec<[Vec3; 3]>,
+) -> Vec<Vec<Vec3>> {
     let mut image: Vec<Vec<Vec3>> = Vec::new();
+    let mut x = x_min;
 
-    let height = 720;
-    let width = 720;
-
-    let mut x = -0.5;
-    while x <= 0.5 {
-        let mut y = -0.5;
+    while x < x_max {
+        let mut y = y_min;
         let mut line: Vec<Vec3> = Vec::new();
-        while y <= 0.5 {
+        while y < y_max {
             let intersect = shape.iter().fold(false, |acc, cur| {
                 acc | (triangle_intersection(
                     Vec3 {
@@ -187,13 +243,59 @@ fn main() {
                     cur,
                 ) != 0.)
             });
-            line.push(Vec3{x: (intersect as i32 as f32)*255., y: 1.0, z: 0.0});
-            
-            y += 1.0/width as f32;
+            line.push(Vec3 {
+                x: (intersect as i32 as f32) * 255.,
+                y: 1.0,
+                z: 0.0,
+            });
+
+            y += 1.0 / width as f32;
         }
         image.push(line);
-        x += 1.0/height as f32;
+        x += 1.0 / height as f32;
     }
+
+    return image;
+}
+
+fn main() {
+    let shape = read_file(Path::new("cow.obj"));
+    // let mut tree = Tree::new();
+    // shape.into_iter().for_each(|elem| tree.insert(elem));
+
+    use std::time::Instant;
+    let now = Instant::now();
+
+    let height = 720;
+    let width = 1280;
+
+    const THREAD_COUNT: usize = 11;
+
+    let x_min = -0.5;
+    let x_max = 0.5;
+    let y_min = -0.5;
+    let y_max = 0.5;
+    
+    let shape_arc = Arc::new(shape);
+    let threads: Vec<std::thread::JoinHandle<Vec<Vec<Vec3>>>> = (0..THREAD_COUNT)
+        .into_iter()
+        .map(|idx| { 
+            let shape_counter = Arc::clone(&shape_arc);
+            thread::spawn(move || {
+                let x_diff = (x_max - x_min) / THREAD_COUNT as f32;
+                let image = get_image_part(x_min + x_diff*idx as f32, x_min + x_diff*(idx as f32 + 1.), y_min, y_max, height, width, &*shape_counter);
+                return image;
+            })
+        })
+        .collect();
+
+    let image = threads.into_iter().fold(Vec::<Vec<Vec3>>::new(), |mut acc, cur| {
+        let current = cur.join().unwrap();
+        acc.splice(acc.len().., current.into_iter());
+        return acc;
+    });
+
+
     let _res = write_to_file(image);
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
