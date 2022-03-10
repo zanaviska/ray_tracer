c3449eeff574c44e21e968c49f8d4a85474296a2
Andras <dlink.uaua@gmail.com>
2022-03-10 20:59:39 +0200

    message

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -182,85 +182,7 @@ fn write_to_file(image: Vec<Vec<Vec3>>) -> std::io::Result<()> {
     Ok(())
 }
 
-enum NextNode {
-    Node(Node),
-    Triangle(Triangle),
-    Nil,
-}
 
-struct Node {
-    min_coor: Vec3,
-    max_coor: Vec3,
-    left: Box<NextNode>,
-    right: Box<NextNode>,
-}
-
-struct Tree {
-    root: Box<NextNode>,
-}
-
-impl Tree {
-    pub fn new() -> Tree {
-        Tree {
-            root: Box::new(NextNode::Node(Node {
-                min_coor: Vec3 {
-                    x: f32::MIN,
-                    y: f32::MIN,
-                    z: f32::MIN,
-                },
-                max_coor: Vec3 {
-                    x: f32::MAX,
-                    y: f32::MAX,
-                    z: f32::MAX,
-                },
-                left: Box::new(NextNode::Nil),
-                right: Box::new(NextNode::Nil),
-            })),
-        }
-    }
-    pub fn insert(&mut self, triangle: Triangle) {
-        let mut old_root = &mut self.root;
-        let return_node = Tree::insert_triangle(&mut old_root, triangle);
-        if let NextNode::Nil = *return_node {
-        } else {
-            let new_root = Box::new(NextNode::Node(Node {
-                min_coor: Vec3 {
-                    x: f32::MIN,
-                    y: f32::MIN,
-                    z: f32::MIN,
-                },
-                max_coor: Vec3 {
-                    x: f32::MAX,
-                    y: f32::MAX,
-                    z: f32::MAX,
-                },
-                left: mem::replace(old_root, Box::new(NextNode::Nil)),
-                right: return_node,
-            }));
-            self.root = new_root;
-        }
-    }
-
-    fn insert_triangle(cur_node: &mut Box<NextNode>, triangle: Triangle) -> Box<NextNode> {
-        let mut new_node = Box::new(NextNode::Nil);
-        let mut return_node = Box::new(NextNode::Nil);
-        match &**cur_node {
-            NextNode::Nil => {
-                println!("Nil");
-                new_node = Box::new(NextNode::Triangle(triangle));
-            }
-            NextNode::Triangle(_old_triangle) => {
-                println!("triangle");
-                return_node = Box::new(NextNode::Triangle(triangle));
-            }
-            NextNode::Node(node) => {
-                println!("node");
-            }
-        }
-        *cur_node = new_node;
-        return return_node;
-    }
-}
 
 fn get_image_part(
     x_min: f32,
@@ -305,43 +227,41 @@ fn get_image_part(
 }
 
 fn main() {
-    let shape = read_file(Path::new("cow1.obj"));
-    let mut tree = Tree::new();
-    shape.into_iter().for_each(|elem| tree.insert(elem));
 
-    // use std::time::Instant;
-    // let now = Instant::now();
+    let shape = read_file(Path::new("cow.obj")); 
+    use std::time::Instant;
+    let now = Instant::now();
 
-    // let height = 720;
-    // let width = 1280;
+    let height = 720;
+    let width = 720;
 
-    // const THREAD_COUNT: usize = 11;
+    const THREAD_COUNT: usize = 11;
 
-    // let x_min = -0.5;
-    // let x_max = 0.5;
-    // let y_min = -0.5;
-    // let y_max = 0.5;
+    let x_min = -0.5;
+    let x_max = 0.5;
+    let y_min = -0.5;
+    let y_max = 0.5;
 
-    // let shape_arc = Arc::new(shape);
-    // let threads: Vec<std::thread::JoinHandle<Vec<Vec<Vec3>>>> = (0..THREAD_COUNT)
-    //     .into_iter()
-    //     .map(|idx| {
-    //         let shape_counter = Arc::clone(&shape_arc);
-    //         thread::spawn(move || {
-    //             let x_diff = (x_max - x_min) / THREAD_COUNT as f32;
-    //             let image = get_image_part(x_min + x_diff*idx as f32, x_min + x_diff*(idx as f32 + 1.), y_min, y_max, height, width, &*shape_counter);
-    //             return image;
-    //         })
-    //     })
-    //     .collect();
+    let shape_arc = Arc::new(shape);
+    let threads: Vec<std::thread::JoinHandle<Vec<Vec<Vec3>>>> = (0..THREAD_COUNT)
+        .into_iter()
+        .map(|idx| {
+            let shape_counter = Arc::clone(&shape_arc);
+            thread::spawn(move || {
+                let x_diff = (x_max - x_min) / THREAD_COUNT as f32;
+                let image = get_image_part(x_min + x_diff*idx as f32, x_min + x_diff*(idx as f32 + 1.), y_min, y_max, height, width, &*shape_counter);
+                return image;
+            })
+        })
+        .collect();
 
-    // let image = threads.into_iter().fold(Vec::<Vec<Vec3>>::new(), |mut acc, cur| {
-    //     let current = cur.join().unwrap();
-    //     acc.splice(acc.len().., current.into_iter());
-    //     return acc;
-    // });
+    let image = threads.into_iter().fold(Vec::<Vec<Vec3>>::new(), |mut acc, cur| {
+        let current = cur.join().unwrap();
+        acc.splice(acc.len().., current.into_iter());
+        return acc;
+    });
 
-    // let _res = write_to_file(image);
-    // let elapsed = now.elapsed();
-    // println!("Elapsed: {:.2?}", elapsed);
+    let _res = write_to_file(image);
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }
